c5645dd0fc23d945a3b8843f2956affd33876b57
nycz <pyntix@gmail.com>
2016-11-21 18:32:03 +0100

    Added scrolling to the TextEdit widget
    also added a little status bar to show the scroll position

--- terminalpana.py
+++ terminalpana.py
@@ -8,43 +8,58 @@ class TextEdit(urwid.Widget):
     line shift to the right when the line is full and the cursor is at the end
     of the line.
     """
-    # TODO: scrolling
-    _sizing = frozenset(['flow'])
+    _sizing = frozenset(['box'])
     _selectable = True
 
     def __init__(self):
         self.edit = urwid.Edit(multiline=True)
+        # The view's offset (in lines)
+        self.offset = 0
+        # The position of the view (0 is at the top, 1 at the bottom)
+        self.scroll_position = 0.0
 
     def keypress(self, size, key):
-        return self.edit.keypress(size, key)
-
-    def rows(self, size, focus=False):
-        return self.edit.rows((size[0]-1,), False)
+        return self.edit.keypress((size[0],), key)
 
     def render(self, size, focus=False):
         """
         Render the edit field with one blank column to the right where the
         cursor can be displayed.
         """
-        canvas = urwid.CompositeCanvas(self.edit.render((size[0]-1,), False))
+        width, height = size
+        canvas = urwid.CompositeCanvas(self.edit.render((width-1,), False))
         canvas.pad_trim_left_right(0, 1)
         # Get the text layout structure
         line_translation = urwid.Text.get_line_translation(self.edit,
-                                                           size[0]-1, None)
+                                                           width-1, None)
         # Calculate the position. Note that the x coordinate is NOT clipped,
         # which means that the cursor will end up in the rightmost column when
         # the line is full instead of staying on the column just to the left
         # of it.
-        canvas.cursor = urwid.text_layout.calc_coords(self.edit.text,
-                                                      line_translation,
-                                                      self.edit.edit_pos)
+        x,y = urwid.text_layout.calc_coords(self.edit.text,
+                                            line_translation,
+                                            self.edit.edit_pos)
+        canvas.cursor = (x,y)
+        if y < self.offset:
+            self.offset = y
+        elif y >= self.offset + height:
+            self.offset = y - height + 1
+        # Only bother calculating scroll position if scrolling is possible
+        if canvas.rows() > height:
+            self.scroll_position = self.offset / (canvas.rows() - height)
+        else:
+            self.scroll_position = 0.0
+        urwid.emit_signal(self, 'scroll_change', str(self.scroll_position))
+        canvas.pad_trim_top_bottom(-self.offset, (self.offset + height) - canvas.rows())
         return canvas
+
 
 def main():
     edit = TextEdit()
-    # temp shit deluxe
-    filler = urwid.Filler(edit, valign='top')
-    frame = urwid.Frame(filler)
+    status = urwid.Text('')
+    urwid.register_signal(TextEdit, ['scroll_change'])
+    urwid.connect_signal(edit, 'scroll_change', status.set_text)
+    frame = urwid.Frame(edit, footer=status)
     loop = urwid.MainLoop(frame)
     loop.run()
 
